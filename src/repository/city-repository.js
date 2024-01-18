5b3277557d070cfb64a4c706a84b2e9008866ef8
aditya <adityasainics10@gmail.com>
2024-01-18 19:45:38 +0530

    completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,28 +1,76 @@
+
 const { City } = require('../models/index');
-
+//index can return all the files in the models folder
 class CityRepository{
-    async createCity({name}){
-        try{
-            const city = await City.crete({name});
-            return city;
-        }catch(error){
-            throw {error};
-        }
+
+   async createCity({ name  }){
+    try{
+
+        const city = await City.create({
+            // model property name: name that has been passed
+            name:name
+        });
+        return city;
+
+    }catch(err){
+
+        console.log('Something went wrong in the repository layer');
+        throw {err};
+
+    }
+   }
+
+   async deleteCity(cityId){
+    try{
+        await City.destroy({
+            where:{
+                id:cityId
+            }
+        });
+        return true;
+
+    }catch(err){
+        console.log('Something went wrong in the repository layer');
+        throw {err};
     }
 
-    async deleteCity({cityId}){
-        try{
-            // to delete   .destroy
-            await City.destroy({
-                where:{
-                    id:cityId
-                }
-            })
+   }
 
-        }catch(error){
-            throw {error};
-        }
+   async updateCity(cityId,data){
+
+    try{
+
+        const city=await City.update(data,{
+            where:{
+                id:cityId
+            }
+        });
+
+        return city;
+
+    }catch(err){
+
+        console.log('Something went wrong in the repository layer');
+        throw {err};
+
     }
+   
+
+   }
+
+   async getCity(cityId){
+    try{
+        const city = await City.findByPk(cityId);
+        return city;
+    }catch(err){
+
+        console.log('Something went wrong in the repository layer');
+        throw {err};
+
+    }
+
+   }
+
 }
 
 module.exports =CityRepository;
