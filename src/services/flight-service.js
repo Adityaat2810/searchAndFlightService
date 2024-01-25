b131e0789b2f193793cff32c8c3a99fa21424535
aditya <adityasainics10@gmail.com>
2024-01-25 23:47:36 +0530

    added one route of flight

---
+++ src/services/flight-service.js
@@ -0,0 +1,49 @@
+const {FlightRepository, AirplaneRepository} = require('../repository/index')
+
+class FlightService{
+
+    constructor(){
+        this.airplaneRepository=new AirplaneRepository()
+        this.flightRepository= new FlightRepository()
+    }
+
+    async createFlight(data){
+        try{
+
+            console.log(data)
+
+            const airplane = await this.airplaneRepository.getAirplane(data.airplaneId);
+            console.log(airplane)
+            const flight = await this.flightRepository.createFlight({...data,totalSeats:airplane.capacity
+            });
+
+            return flight ;
+
+        }catch(error){
+
+            console.log("something went wrong at service layer");
+            throw{error};
+
+        }
+    }
+
+    async getFlightData(){
+        //todo
+    }
+}
+
+module.exports  =FlightService
+
+/**
+ * {
+ *  flightNumber,
+ *  airplaneId ,
+ *  departureAirportId,
+ *  arrivalAirportId,
+ *  arrivalTime,
+ *  departureTime,
+ *  price ,
+ *  totalSeats ->airplane
+ *  
+ * }
+ */
